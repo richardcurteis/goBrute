6762ab0191aa19d013f55aeb250de8a6c0862f1b
3therk1ll <richardcurteis@users.noreply.github.com>
2021-03-27 16:34:09 +0000

    Create testServer.php

---
+++ testServer.php
@@ -0,0 +1,27 @@
+<?php
+    // curl --header "Content-Type: application/json" --request POST  --data '{"value":"313375"}' http://your_server_ip/index.php
+    // $otp_token = strval(mt_rand(000000, 999999));
+    // echo $otp_token;
+    echo "Test Server. Don't waste your time.\n";
+    $otp_token = "000011";
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json);
+        try {
+            $user_otp = $data->value;
+        } catch (Exception $e){
+            die();
+        }
+    } else {
+        die();
+    }
+
+    if ($user_otp === $otp_token) {
+        setcookie('bro_server', 'otp_has_been_found');
+        $data = "null";
+    } else {
+        $data = '{"detail": {"exception": "InvalidOTPValueError", "message": "Invalid 2FA value"}}';
+    }
+    header('Content-Type: application/json');
+    echo json_encode($data);    
+?>
